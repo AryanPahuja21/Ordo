fcf179c1de5e3eb98341cdecb6faa8280b1e3e5c
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-10-23 02:31:14 +0530

    custom component styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold">Ordo</h1>
-      <Button>Join now</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
