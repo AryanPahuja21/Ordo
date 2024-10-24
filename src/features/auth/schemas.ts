0716df4d3413d3a5e2f1c823e3d639ca48311535
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-10-25 03:03:47 +0530

    auth logic with hono integration

--- src/features/auth/schemas.ts
+++ src/features/auth/schemas.ts
@@ -4,3 +4,9 @@ export const loginSchema = z.object({
   email: z.string().trim().email(),
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
+
+export const registerSchema = z.object({
+  name: z.string().trim().min(1, "Name is required"),
+  email: z.string().email(),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
