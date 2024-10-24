b8376ad216d28c7ea63a92f6016655a75d81707f
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-10-25 05:22:42 +0530

    appwrite setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold">Ordo</h1>
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
