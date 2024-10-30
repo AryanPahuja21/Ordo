710a4c62d8b0bf2b5afc372f6f98ed0242f5aa11
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-10-31 02:53:09 +0530

    members access logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
   title: "Ordo - Organize Work, Empower Teams",
   description:
     "Ordo is a simple, powerful project management tool that helps teams organize tasks, streamline workflows, and stay aligned. Bring clarity to your projects and achieve more with Ordo's intuitive, efficient platform.",
+  icons: ["/logo.svg"],
 };
 
 export default function RootLayout({
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export const Logo = () => {
   return (
     <Link href="/">
-      <div className="flex items-center gap-1">
+      <div className="flex items-center gap-1 select-none">
         {" "}
         <Image src="/logo.svg" alt="Ordo" width={50} height={50} />
         <h1 className="text-2xl font-bold font-sans">ORDO</h1>
