279bbc9ba6c9d67e475f455162186568d1f754de
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-11-12 04:54:30 +0530

    members page ui

--- src/features/members/components/members-list.tsx
+++ src/features/members/components/members-list.tsx
@@ -4,10 +4,18 @@ import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
-import { ArrowLeftIcon } from "lucide-react";
+import { ArrowLeftIcon, MoreVerticalIcon } from "lucide-react";
 import Link from "next/link";
-import { useGetMembers } from "../api/use-get-workspaces";
+import { useGetMembers } from "../api/use-get-members";
 import { Fragment } from "react";
+import { MemberAvatar } from "./members-avatar";
+import { Separator } from "@/components/ui/separator";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
 
 export const MembersList = () => {
   const workspaceId = useWorkspaceId();
@@ -28,9 +36,52 @@ export const MembersList = () => {
         <DottedSeparator />
       </div>
       <CardContent className="p-7">
-        {data?.documents.map((member) => (
+        {data?.documents.map((member, index) => (
           <Fragment key={member.$id}>
-            <div className="flex items-center gap-2">Member</div>
+            <div className="flex items-center gap-2">
+              <MemberAvatar
+                className="size-10"
+                fallbackClassName="text-lg"
+                name={member.name}
+              />
+              <div className="flex flex-col">
+                <p className="text-sm font-medium">{member.name}</p>
+                <p className="text-xs text-muted-foreground ">{member.email}</p>
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button className="ml-auto" variant="secondary" size="icon">
+                    <MoreVerticalIcon />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side="bottom" align="end">
+                  <DropdownMenuItem
+                    className="font-medium"
+                    onClick={() => {}}
+                    disabled={false}
+                  >
+                    Set as Administrator
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="font-medium"
+                    onClick={() => {}}
+                    disabled={false}
+                  >
+                    Set as Member
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="font-medium text-amber-700"
+                    onClick={() => {}}
+                    disabled={false}
+                  >
+                    Remove Member
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            {index < data.documents.length - 1 && (
+              <Separator className="my-2.5" />
+            )}
           </Fragment>
         ))}
       </CardContent>
